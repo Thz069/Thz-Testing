c212a067af58f8acf19b44517aa00dd1318e542d
Thz069 <victor.thoma@gmail.com>
2017-07-28 13:56:12 -0300

    Adição de javascrip
    adição de javascript e continuação do style css e do index

---
+++ cemjs.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function mydiv() {
+    document.getElementById("divimgdrop").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var divimgdrop = document.getElementById("divimgdrop");
+      if (divimgdrop.classList.contains('show')) {
+        divimgdrop.classList.remove('show');
+      }
+  }
+}
